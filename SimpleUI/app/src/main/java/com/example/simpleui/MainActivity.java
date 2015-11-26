127238a2551bf02573788fc037af9e6ab67e3a1f
kungtsan <kungtsan@gmail.com>
2015-11-26 21:56:23 +0800

    20151126

--- SimpleUI/app/src/main/java/com/example/simpleui/MainActivity.java
+++ SimpleUI/app/src/main/java/com/example/simpleui/MainActivity.java
@@ -2,12 +2,46 @@ package com.example.simpleui;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.KeyEvent;
+import android.view.View;
+import android.widget.CheckBox;
+import android.widget.EditText;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
+
+    private EditText inputText;
+    private CheckBox hideCheckBox;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        inputText = (EditText)findViewById(R.id.inputText);
+        inputText.setText("1234");
+        inputText.setOnKeyListener(new View.OnKeyListener() {
+            @Override
+            public boolean onKey(View v, int keyCode, KeyEvent event) {
+                if(event.getAction() == KeyEvent.ACTION_DOWN){
+                    if(keyCode == KeyEvent.KEYCODE_ENTER){
+                        submit(v);
+                        return true;
+                    }
+                }
+                return false;
+            }
+        });
+
+        hideCheckBox = (CheckBox) findViewById(R.id.hideCheckBox);
+        hideCheckBox.setChecked(true);
+    }
+    public void submit(View view){
+        String text = inputText.getText().toString();
+        if(hideCheckBox.isChecked()){
+            text = "*******************";
+            inputText.setText("**********************");
+        }
+        Toast.makeText(this,text, Toast.LENGTH_LONG).show();
+        inputText.setText("");
     }
 }
